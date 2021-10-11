44602ccd17d75a9a4fd93f43b0a59643b766d429
Nadine <nadinelao1224@yahoo.com>
2021-10-11 23:48:29 +0100

    more tests

--- lca.java
+++ lca.java
@@ -70,12 +70,12 @@ public class lca
         tree.root=new Node(1);
         tree.root.left=new Node(2);
         tree.root.right=new Node(3);
-        tree.root.left.left=new Node(4);
-        tree.root.left.right=new Node(5);
-        tree.root.right.left=new Node(6);
-        tree.root.right.right=new Node(7);
+		tree.root.right.right = new Node(4);
+		tree.root.right.right.right = new Node (5);
+		tree.root.right.right.right.right = new Node(6);
+		tree.root.right.right.right.right.right = new Node (7);
 
-        System.out.println("LCA(5, 7): " + tree.findLCA(5,7)); 
+        System.out.println("LCA(4, 5): " + tree.findLCA(6,7)); 
 
       
     } 
--- lcaTest.java
+++ lcaTest.java
@@ -17,14 +17,14 @@ public class lcaTest {
         tree.root.right.left=new Node(6);
         tree.root.right.right=new Node(7);
 
-        assertEquals("LCA (5,7) is 1",1, tree.findLCA(5,7));
+        assertEquals("LCA (5,7) = 1",1, tree.findLCA(5,7));
     }
 
     @Test
 	public void testEmptyTree() {
 		lca tree =new lca();
 	
-		assertEquals("LCA (1,2)=-1",-1, tree.findLCA(1,2));
+		assertEquals("LCA (1,2) = -1",-1, tree.findLCA(1,2));
 	}
 
     @Test
@@ -34,8 +34,42 @@ public class lcaTest {
         tree.root.left=new Node(2);
         tree.root.right=new Node(3);
 		
-		assertEquals("LCA of (1,4) is -1 ",-1, tree.findLCA(1,5));
+		assertEquals("LCA of (1,4) = -1 ",-1, tree.findLCA(1,5));
 		
+	}
+
+
+    @Test
+	public void testLeftLeaningTree() {
+		lca tree =new lca();
+        tree.root=new Node(1);
+        tree.root.left=new Node(2);
+        tree.root.right=new Node(3);
+		tree.root.left.left = new Node(4);
+		tree.root.left.left.left = new Node(5);
+		tree.root.left.left.left.left = new Node(6);
+		tree.root.left.left.left.left.left = new Node(7);
+		assertEquals("LCA of (1,2) = 1",1, tree.findLCA(1,2));
+		assertEquals("LCA of (3,4) = 1",1, tree.findLCA(3,4));
+		assertEquals("LCA of (6,7) = 6",6, tree.findLCA(6,7));
+			
+	}
+
+    @Test
+	public void testRightLeaningTree() {
+		lca tree =new lca();
+        tree.root=new Node(1);
+        tree.root.left=new Node(2);
+        tree.root.right=new Node(3);
+		tree.root.right.right = new Node(4);
+		tree.root.right.right.right = new Node (5);
+		tree.root.right.right.right.right = new Node(6);
+		tree.root.right.right.right.right.right = new Node (7);
+		
+		assertEquals("LCA (1,2) = 1",1, tree.findLCA(1,2));
+		assertEquals("LCA (2,4) = 1",1, tree.findLCA(2,4));
+		assertEquals("LCA (4,5) = 4",4, tree.findLCA(4,5));
+
 	}
 
 }
